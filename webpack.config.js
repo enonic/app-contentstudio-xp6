f1f193514c3ce01b351b8c09120302fd2d99970d
Alan Semenov <ase@enonic.com>
2021-05-31 17:37:46 +0200

    Testing PR merger

--- webpack.config.js
+++ webpack.config.js
@@ -65,11 +65,9 @@ module.exports = {
             filename: '[name].css',
             chunkFilename: './styles/[id].css'
         }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {from: 'icons/fonts/icomoon.*', to: 'page-editor/fonts/[name].[ext]'}
-            ]
-        }),
+        new CopyWebpackPlugin([
+            { from: 'icons/fonts/icomoon.*', to: 'page-editor/fonts/[name].[ext]' }
+        ]),
         new CircularDependencyPlugin({
             exclude: /a\.js|node_modules/,
             failOnError: true
